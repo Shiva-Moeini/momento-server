a213756b4afffb2c4655fa9921a454eec6dbd981
Sheev <shiva.moeini20@gmail.com>
2025-01-13 12:22:39 +0100

    Added server-side routes for timelines and items with CRUD operations

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,40 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/timelines', (c) => {
+  return c.json({})
+})
+
+app.post('/timelines', (c) => {
+  return c.json({})
+})
+
+app.put('/timelines', (c) => {
+  return c.json({})
+})
+
+app.delete('/timelines', (c) => {
+  return c.json({})
+})
+
+app.get('/timelines/:id', (c) => {
+  return c.json({})
+})
+
+app.get('/timelines/:id/timeline-items', (c) => {
+  return c.json({})
+})
+
+app.post('/timelines/:id/timeline-items', (c) => {
+  return c.json({})
+})
+
+app.put('/timelines/:id/timeline-items', (c) => {
+  return c.json({})
+})
+
+app.delete('/timelines/:id/timeline-items', (c) => {
+  return c.json({})
 })
 
 const port = 3000
